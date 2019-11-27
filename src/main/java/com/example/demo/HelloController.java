601d446707ff8235c0137ce518a77672d4b3b852
Joey Zhang <joey.zhang@tgitech.net>
2019-11-28 01:45:18 +0800

    fix: whitelabel error

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
