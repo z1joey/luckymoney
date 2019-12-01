9832418e70b2ce4e579e3d3725b7e37ed8e06eda
Joey Zhang <joey.zhang@tgitech.net>
2019-12-01 20:50:22 +0800

    feat: create, search, update

--- src/main/java/com/example/demo/LuckymoneyController.java
+++ src/main/java/com/example/demo/LuckymoneyController.java
@@ -1,10 +1,11 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author yizhang
@@ -19,4 +20,45 @@ public class LuckymoneyController {
     public List<Luckymoney> list() {
         return repository.findAll();
     }
+
+    /**
+     * Create a luckymoney
+     */
+    @PostMapping("/luckymoneys")
+    public  Luckymoney create(@RequestParam("producer") String producer,
+                              @RequestParam("money") BigDecimal money) {
+        Luckymoney luckymoney = new Luckymoney();
+        luckymoney.setProducer(producer);
+        luckymoney.setMoney(money);
+        return repository.save(luckymoney);
+    }
+
+    /**
+     * Search a luckymoney by Id
+     */
+    @GetMapping("/luckymoneys/{id}")
+    public Luckymoney findById(@PathVariable("id") Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    /**
+     * Update a luckmoney
+     * Put and Get might not share the same path (unconfirmed)
+     * If the Id is not given, will generate a new one
+     * The following update is actually create a new item with the same id
+     */
+    @PutMapping("/update/{id}")
+    public Luckymoney update(@PathVariable("id") Integer id,
+                             @RequestParam("consumer") String consumer) {
+
+        Optional<Luckymoney> optional = repository.findById(id);
+        if (optional.isPresent()) {
+            Luckymoney luckymoney = optional.get();
+            luckymoney.setConsumer(consumer);
+            return repository.save(luckymoney);
+        }
+
+        return null;
+    }
+
 }
